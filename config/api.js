e92058f5161eb8219f95dc8864f72d4ae3c00981
Max <404924175@qq.com>
2018-11-05 11:00:07 +0800

    封装基础方法

---
+++ config/api.js
@@ -0,0 +1,5 @@
+const API_URL = 'http://192.168.1.1'
+
+module.exports = {
+    auth: API_URL + '/ndltd-virtual-value/api/wx/auth'
+}
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,67 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+const showToast = (title) => {
+    wx.showToast({
+        title: title || '网络异常，请重试',
+        icon: 'none',
+        duration: 2500
+    })
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+function request(url, data, method = 'GET') {
+    wx.showLoading({
+        title: '加载中...',
+    })
+
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url,
+            method,
+            header: {
+                'Content-Type': 'application/json'
+            },
+            data,
+            success(res) {
+                wx.hideLoading()
+                if (res.statusCode === 200) {
+                    if (res.data.code !== 0) {
+                        showToast(res.data.msg)
+                        reject(res)
+                    } else {
+                        resolve(res)
+                    }
+                } else {
+                    showToast()
+                    reject(res)
+                }
+            },
+            fail(err) {
+                wx.hideLoading()
+                showToast()
+                reject(err)
+                console.log('failed')
+            }
+        })
+    })
+}
+
+function getUserInfo() {
+    return new Promise((resolve, reject) => {
+        wx.getSetting({
+            success: (res) => {
+                if (res.authSetting['scope.userInfo']) {
+                    wx.getUserInfo({
+                        success: (res) => {
+                            resolve(res)
+                        }
+                    })
+                } else {
+                    reject(res)
+                }
+            }
+        })
+    })
 }
 
 module.exports = {
-  formatTime: formatTime
+    request,
+    getUserInfo
 }
